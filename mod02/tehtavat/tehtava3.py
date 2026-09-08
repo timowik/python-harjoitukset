d168d5720a2d1e8f997919e8fa22909c03311df1
Timo <timo.wikstrom@metropolia.fi>
2026-09-08 20:42:06 +0300

    commit

---
+++ mod02/tehtavat/tehtava3.py
@@ -0,0 +1,8 @@
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+
+piiri = 2 * (kanta + korkeus)
+pinta_ala = kanta * korkeus
+
+print("Piiri:", piiri)
+print("Pinta-ala:", pinta_ala)
