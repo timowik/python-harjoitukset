31a70a0297231ce44e512a519f2033bde6b29ada
Timo <timo.wikstrom@metropolia.fi>
2026-09-08 21:24:23 +0300

    commit

---
+++ mod02/tehtavat/tehtava4.py
@@ -0,0 +1,11 @@
+luku1 = int(input("Anna ensimmäinen kokonaisluku: "))
+luku2 = int(input("Anna toinen kokonaisluku: "))
+luku3 = int(input("Anna kolmas kokonaisluku: "))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+
+print("Summa:", summa)
+print("Tulo:", tulo)
+print("Keskiarvo:", keskiarvo)
